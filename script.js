1c81c34874f6847567e5336dfee2eb4a4e1530c7
Technicalearth <74358289+Technicalearth@users.noreply.github.com>
2021-12-05 14:02:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let place = document.getElementById("place")
+let counter = 0;
+let places = [ "Andhra Pradesh",
+"Arunachal Pradesh",
+"Assam",
+"Bihar",
+"Chhattisgarh",
+"Goa",
+"Gujarat",
+"Haryana",
+"Himachal Pradesh",
+"Jammu and Kashmir",
+"Jharkhand",
+"Karnataka",
+"Kerala",
+"Madhya Pradesh",
+"Maharashtra",
+"Manipur",
+"Meghalaya",
+"Mizoram",
+"Nagaland",
+"Odisha",
+"Punjab",
+"Rajasthan",
+"Sikkim",
+"Tamil Nadu",
+"Telangana",
+"Tripura",
+"Uttarakhand",
+"Uttar Pradesh",
+"West Bengal",
+"Andaman and Nicobar Islands",
+"Chandigarh",
+"Dadra and Nagar Haveli",
+"Daman and Diu",
+"Delhi",
+"Lakshadweep",
+"Puducherry"]
+
+setInterval(() => {
+    place.innerText = places[counter]
+    counter++
+    if (counter >= places.length) {
+        counter = 0
+    }
+}, 5000);
